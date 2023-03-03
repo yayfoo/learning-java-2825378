a5476f3abf971543892921dc867851d396767e14
yayfoo <yayfoo@users.noreply.github.com>
2023-03-03 09:16:09 +0000

    Solve "Multiple choice" challenge

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        String question = "What is the largest planet in our solar system?";
-        String choiceOne = "earth";
-        String choiceTwo = "jupiter";
-        String choiceThree = "saturn";
+        String question = "What fruit contains most sugar?";
+        String choiceOne = "apples";
+        String choiceTwo = "kiwis";
+        String choiceThree = "bananas";
 
-        String correctAnswer = choiceTwo;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
         System.out.println(question);
-
         // Write a print statement giving the answer choices
-        System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
+        System.out.println(choiceOne + ", " + choiceTwo + " or " + choiceThree + "?");
 
         // Have the user input an answer
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         // Retrieve the user's input
-        String input = scanner.next();
+        String userInput = input.next();
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-        if(correctAnswer.equals(input.toLowerCase())) {
-            System.out.println("Congrats! That's the correct answer");
-        } else {
-            System.out.println("You are incorrect. The correct answer is " + correctAnswer);
+        if (userInput.equals(correctAnswer)) {
+            System.out.println("Congrats, " + userInput + " is the correct answer!");
         }
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+        else {
+            System.out.println("Sorry, " + userInput + " is incorrect. The correct answer is " + correctAnswer + ".");
+        }
 
     }
 
